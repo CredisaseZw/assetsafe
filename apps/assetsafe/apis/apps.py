0c94dde4066b563eb0ee8da5551b3417c574cf1e
Seh Maluva <sehmaluva@gmail.com>
2026-04-01 08:51:54 +0200

    docs(assetsafe): add docstrings to api app configuration and views

--- apps/assetsafe/apis/apps.py
+++ apps/assetsafe/apis/apps.py
@@ -1,6 +1,10 @@
+"""Assetsafe API app configuration."""
+
 from django.apps import AppConfig
 
 
 class AssetsafeConfig(AppConfig):
+    """Assetsafe API app configuration."""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.assetsafe"
