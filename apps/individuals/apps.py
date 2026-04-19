26f8b50fa9a8d3f5a4da90ecb40f528f2a0e7c4f
Malvin T. Machingura <sehmaluva@gmail.com>
2026-04-19 08:27:54 +0200

    fix(individuals): standardize string quotes in IndividualsConfig (#139)

--- apps/individuals/apps.py
+++ apps/individuals/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class IndividualsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.individuals'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.individuals"
+    verbose_name = "Individuals"
