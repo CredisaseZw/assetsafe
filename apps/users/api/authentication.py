fc989c4c3f6e2eb1bdfba7a660af62be14506d11
Malvin T. Machingura <sehmaluva@gmail.com>
2026-04-21 08:58:30 +0200

    fix(authentication): standardize string quotes in CookieJWTAuthentication (#142)

--- apps/users/api/authentication.py
+++ apps/users/api/authentication.py
@@ -3,10 +3,11 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.exceptions import InvalidToken, AuthenticationFailed
 from django.conf import settings
 
+
 class CookieJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
         if access_token := request.COOKIES.get(
-            settings.SIMPLE_JWT.get('AUTH_COOKIE', 'access_token')
+            settings.SIMPLE_JWT.get("AUTH_COOKIE", "access_token")
         ):
             try:
                 # Validate the token from cookie
