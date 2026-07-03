d9be66065c480e40dfe6bc5a7d1844ad8bb9f865
Seh Maluva <sehmaluva@gmail.com>
2026-07-03 20:45:20 +0200

    feat: update subscription date column headers in asset registry table

--- frontend/src/pages/AssetRegistryPage.tsx
+++ frontend/src/pages/AssetRegistryPage.tsx
@@ -260,8 +260,12 @@ export default function AssetRegistryPage() {
                   <th className="px-2 py-2 font-bold text-black text-right">
                     Est. Value
                   </th>
-                  <th className="px-2 py-2 font-bold text-black">Sub. Start</th>
-                  <th className="px-2 py-2 font-bold text-black">Sub. End</th>
+                  <th className="px-2 py-2 font-bold text-black">
+                    Sub. Start Date
+                  </th>
+                  <th className="px-2 py-2 font-bold text-black">
+                    Sub. End Date
+                  </th>
                   <th className="px-2 py-2 font-bold text-black" />
                 </tr>
               </thead>
